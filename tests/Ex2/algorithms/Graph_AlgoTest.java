7891d72182f2d09a76b0ac8d9c100ebf6f480d15
Aviel Yosef <ayosef@redhat.com>
2019-12-31 13:19:16 +0200

    Adding Ex2 test samples

---
+++ tests/Ex2/algorithms/Graph_AlgoTest.java
@@ -0,0 +1,14 @@
+package Ex2.algorithms;
+
+import Ex2.dataStructure.graph;
+import Ex2.dataStructure.node_data;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Graph_AlgoTest {
+    @Test
+    public void justTrue() {
+        assertTrue(true);
+    }
+} 
---
+++ tests/Ex2/dataStructure/DGraphTest.java
@@ -0,0 +1,11 @@
+package Ex2.dataStructure;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DGraphTest {
+    @Test
+    public void justTrue() {
+        assertTrue(true);
+    }
+} 
